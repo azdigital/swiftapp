aa49ae7c95b22653da84cef591dc58945b8c5650
azdigital <andrew@azdigital.ca>
2016-09-03 19:26:23 -0400

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var theLabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        
+           theLabel.text = "Hello there!"
+        print("Button tapped")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
     }
 
     override func didReceiveMemoryWarning() {
